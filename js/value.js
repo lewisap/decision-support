96848b96b9f52f4cb2d74ae608d6932a19333af1
Adam Lewis <adam.lewis@marklogic.com>
2015-08-12 12:24:53 -0400

    added initial code to populate fields based on saved metrics, and save a more fully populated obj to the database

--- js/value.js
+++ js/value.js
@@ -1,4 +1,5 @@
 var current, widget, btnnext, btnback, btnsubmit;
+var savedMetrics = [];
 
 var hideButtons = function (current) {
     "use strict";
@@ -6,17 +7,33 @@ var hideButtons = function (current) {
 
     $(".action").hide();
 
-    if (current < limit) btnnext.show();
-    if (current > 1) btnback.show();
-    if (current == limit) {
+    if (current < limit) {
+        btnnext.show();
+    }
+
+    if (current > 1) {
+        btnback.show();
+    }
+
+    if (current === limit) {
         btnnext.hide();
         btnsubmit.show();
     }
-}
+};
+
+// Change progress bar action
+var setProgress = function (currstep) {
+    "use strict";
+    var percent = parseFloat(100 / widget.length) * currstep;
+    percent = percent.toFixed();
+    $(".progress-bar")
+        .css("width", percent + "%")
+        .html(percent + "% Complete");
+};
 
 $(document).ready(function () {
     "use strict";
-    
+
     current = 1;
     widget = $(".step");
     btnnext = $(".next");
@@ -36,7 +53,7 @@ $(document).ready(function () {
             setProgress(current);
         }
         hideButtons(current);
-    })
+    });
 
     // Back button click action 	
     btnback.click(function () {
@@ -45,38 +62,66 @@ $(document).ready(function () {
             btnnext.trigger('click');
         }
         hideButtons(current);
-    })
-});
+    });
 
-// Change progress bar action
-setProgress = function (currstep) {
-    var percent = parseFloat(100 / widget.length) * currstep;
-    percent = percent.toFixed();
-    $(".progress-bar")
-        .css("width", percent + "%")
-        .html(percent + "% Complete");
-}
+    // save the metric to JS array
+    $('#addMetric').click(function (e) {
+        var name = $('#metricName').val();
+        var objective = $('#txtObjective').val();
+        var goal = $('#txtGoal').val();
+        var assumptions = $('#txtAssumptions').val();
+        var considerations = $('#txtConsiderations').val();
 
-// Hide buttons according to the current step
+        var list = $('#savedMetrics');
+        var numSavedItems = $("#savedMetrics [href]").length;
 
-// save the metric to hidden fields and manage the form appropriately
-$('#addMetric').click(function (e) {
-    var name = $('#metricName').val();
-    var list = $('#savedMetrics');
-    list.show();
-    list.append('<a href="#" class="list-group-item">' + name + '</a>');
+        list.show();
+        list.append('<a href="#" class="list-group-item savedMetric" id="metric-' + numSavedItems + '">' + name + '</a>');
 
-    var numSavedItems = $("#savedMetrics [href]").length;
+        // save the metric to the metrics JS array as an object
+        var metric = {};
+        metric.name = name;
+        metric.objective = objective;
+        metric.goal = goal;
+        metric.assumptions = assumptions;
+        metric.considerations = considerations;
 
-    // disable the add button once we hit 3 saved metrics
-    if (numSavedItems >= 3) {
-        $(this).prop("disabled", true);
-    }
+        savedMetrics[numSavedItems] = metric;
 
-    $("#frmMetrics").trigger('reset');
-});
+        // disable the add button once we hit 3 saved metrics (0 based)
+        if (numSavedItems >= 2) {
+            $(this).prop("disabled", true);
+        }
 
-$('#btnStart').click(function (e) {
-    $('#divTitle').hide();
-    $('#divWorksheet').show("fast");
+        $("#frmMetrics").trigger('reset');
+    });
+
+    // click handler for user selecing an already saved metric
+    $('#savedMetrics').on('click', 'a', function () {
+        var arrNum = $(this).attr('id').split('-')[1];
+        var obj = savedMetrics[arrNum];
+        
+        $('#metricName').val(obj.name);
+        $('#txtObjective').val(obj.objective);
+        $('#txtGoal').val(obj.goal);
+        $('#txtAssumptions').val(obj.assumptions);
+        $('#txtConsiderations').val(obj.considerations);
+    });
+
+    $('#btnStart').click(function (e) {
+        $('#divTitle').hide();
+        $('#divWorksheet').show("fast");
+    });
+
+    $('#btnSubmit').click(function (e) {
+        e.preventDefault(); // don't auto-submit the form and navigate on
+        $.ajax({
+            type: "PUT",
+            contentType: 'application/json',
+            url: "/v1/documents?uri=example.json",
+            data: JSON.stringify(savedMetrics),
+        }).done(function (msg) {
+            alert("Your data has been saved to the server");
+        });
+    });
 });
